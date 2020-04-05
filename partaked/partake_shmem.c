1cbdd9358abcec72dd5e011435de17dc3819a00b
Mark A. Tsuchida <matsuchida@wisc.edu>
2020-04-05 15:36:48 -0500

    Simplify random name generation on Unix

--- partaked/partake_shmem.c
+++ partaked/partake_shmem.c
@@ -37,47 +37,26 @@
 
 #include <zf_log.h>
 
-#ifndef _WIN32
-#   include <errno.h>
-#   include <fcntl.h>
-#   include <unistd.h>
-#else
+#ifdef _WIN32
 #   include <Bcrypt.h>
+#endif
+
+#ifdef __linux__
+#   include <bsd/stdlib.h> // arc4random
 #endif
 
 #include <stdlib.h>
 
 
 #define NAME_INFIX PARTAKE_TEXT("partake-shmem-")
-#define DEV_URANDOM "/dev/urandom"
 
 
 static char *alloc_random_bytes(size_t size) {
     char *buf = partake_malloc(size);
 
 #ifndef _WIN32
-    char emsg[1024];
-    errno = 0;
-    int fd = open(DEV_URANDOM, O_RDONLY);
-    if (fd < 0) {
-        ZF_LOGF("open: " DEV_URANDOM ": %s",
-                partake_strerror(errno, emsg, sizeof(emsg)));
-        abort();
-    }
-
-    errno = 0;
-    ssize_t bytes = read(fd, buf, size);
-    if (bytes < size) {
-        ZF_LOGF("read: " DEV_URANDOM ": %s",
-                partake_strerror(errno, emsg, sizeof(emsg)));
-        abort();
-    }
-
-    errno = 0;
-    if (close(fd) != 0) {
-        ZF_LOGF("close: " DEV_URANDOM ": %s",
-                partake_strerror(errno, emsg, sizeof(emsg)));
-    }
+    // Not POSIX but available on Linux, macOS, most BSDs
+    arc4random_buf(buf, size);
 #else
     NTSTATUS ret = BCryptGenRandom(NULL, buf, size,
             BCRYPT_USE_SYSTEM_PREFERRED_RNG);
