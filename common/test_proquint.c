ad279d2e98b4f7fc68394df23f584c55da3f9f7d
Mark A. Tsuchida <matsuchida@wisc.edu>
2021-06-19 17:11:54 -0500

    Unit test setup

---
+++ common/test_proquint.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2020-2021 Board of Regents of the University of Wisconsin System
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <unity.h>
+
+void setUp(void) { }
+void tearDown(void) { }
+
+int main() {
+    UNITY_BEGIN();
+    return UNITY_END();
+}
