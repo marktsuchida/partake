d4e4c41850c10a3f9dc73e42c669060d3e266b2a
Mark A. Tsuchida <matsuchida@wisc.edu>
2021-06-19 17:53:10 -0500

    Test and fix proquint conversions

--- common/partake_proquint.c
+++ common/partake_proquint.c
@@ -56,22 +56,22 @@ void partake_proquint_from_uint64(uint64_t i, char *dest24) {
 static inline int consonant_to_4bits(char ch, uint16_t *dest) {
     *dest <<= 4;
     switch (ch) {
-    case 'b': *dest &= 0x0; break;
-    case 'd': *dest &= 0x1; break;
-    case 'f': *dest &= 0x2; break;
-    case 'g': *dest &= 0x3; break;
-    case 'h': *dest &= 0x4; break;
-    case 'j': *dest &= 0x5; break;
-    case 'k': *dest &= 0x6; break;
-    case 'l': *dest &= 0x7; break;
-    case 'm': *dest &= 0x8; break;
-    case 'n': *dest &= 0x9; break;
-    case 'p': *dest &= 0xA; break;
-    case 'r': *dest &= 0xB; break;
-    case 's': *dest &= 0xC; break;
-    case 't': *dest &= 0xD; break;
-    case 'v': *dest &= 0xE; break;
-    case 'z': *dest &= 0xF; break;
+    case 'b': *dest |= 0x0; break;
+    case 'd': *dest |= 0x1; break;
+    case 'f': *dest |= 0x2; break;
+    case 'g': *dest |= 0x3; break;
+    case 'h': *dest |= 0x4; break;
+    case 'j': *dest |= 0x5; break;
+    case 'k': *dest |= 0x6; break;
+    case 'l': *dest |= 0x7; break;
+    case 'm': *dest |= 0x8; break;
+    case 'n': *dest |= 0x9; break;
+    case 'p': *dest |= 0xA; break;
+    case 'r': *dest |= 0xB; break;
+    case 's': *dest |= 0xC; break;
+    case 't': *dest |= 0xD; break;
+    case 'v': *dest |= 0xE; break;
+    case 'z': *dest |= 0xF; break;
     default: return ERROR;
     }
     return 0;
@@ -81,10 +81,10 @@ static inline int consonant_to_4bits(char ch, uint16_t *dest) {
 static inline int vowel_to_2bits(char ch, uint16_t *dest) {
     *dest <<= 2;
     switch (ch) {
-    case 'a': *dest &= 0x0; break;
-    case 'i': *dest &= 0x1; break;
-    case 'o': *dest &= 0x2; break;
-    case 'u': *dest &= 0x3; break;
+    case 'a': *dest |= 0x0; break;
+    case 'i': *dest |= 0x1; break;
+    case 'o': *dest |= 0x2; break;
+    case 'u': *dest |= 0x3; break;
     default: return ERROR;
     }
     return 0;
@@ -112,33 +112,38 @@ int partake_proquint_to_uint64(const char *pq, uint64_t *dest) {
     *dest = 0;
     uint16_t word;
     int err;
+
     if ((err = proquint_to_uint16(pq, &word)) != 0)
         return err;
-    *dest &= word;
     *dest <<= 16;
+    *dest |= word;
     pq += 5;
     if (*pq++ != '-')
         return ERROR;
+
     if ((err = proquint_to_uint16(pq, &word)) != 0)
         return err;
-    *dest &= word;
     *dest <<= 16;
+    *dest |= word;
     pq += 5;
     if (*pq++ != '-')
         return ERROR;
+
     if ((err = proquint_to_uint16(pq, &word)) != 0)
         return err;
-    *dest &= word;
     *dest <<= 16;
+    *dest |= word;
     pq += 5;
     if (*pq++ != '-')
         return ERROR;
+
     if ((err = proquint_to_uint16(pq, &word)) != 0)
         return err;
-    *dest &= word;
     *dest <<= 16;
+    *dest |= word;
     pq += 5;
     if (*pq != '\0')
         return ERROR;
+
     return 0;
 }
--- common/test_proquint.c
+++ common/test_proquint.c
@@ -5,10 +5,110 @@
 
 #include <unity.h>
 
+#include "partake_proquint.c"
+
 void setUp(void) { }
 void tearDown(void) { }
 
+
+void test_to_proquint(void) {
+    uint64_t i;
+    char pq[24];
+
+    i = 0;
+    partake_proquint_from_uint64(i, pq);
+    TEST_ASSERT_EQUAL_STRING("babab-babab-babab-babab", pq);
+
+    i = -1;
+    partake_proquint_from_uint64(i, pq);
+    TEST_ASSERT_EQUAL_STRING("zuzuz-zuzuz-zuzuz-zuzuz", pq);
+
+    i = 0x3F54DCC18C62C18D;
+    partake_proquint_from_uint64(i, pq);
+    TEST_ASSERT_EQUAL_STRING("gutih-tugad-mudof-sakat", pq);
+}
+
+
+void test_16_from_proquint(void) {
+    uint16_t i;
+    int err;
+
+    err = proquint_to_uint16("babab", &i);
+    TEST_ASSERT_EQUAL_INT(0, err);
+    TEST_ASSERT_EQUAL_UINT64(0, i);
+
+    err = proquint_to_uint16("zuzuz", &i);
+    TEST_ASSERT_EQUAL_INT(0, err);
+    TEST_ASSERT_EQUAL_UINT64(0xFFFF, i);
+
+    err = proquint_to_uint16("gutih", &i);
+    TEST_ASSERT_EQUAL_INT(0, err);
+    TEST_ASSERT_EQUAL_UINT64(0x3F54, i);
+}
+
+
+void test_from_proquint(void) {
+    uint64_t i;
+    int err;
+
+    err = partake_proquint_to_uint64("babab-babab-babab-babab", &i);
+    TEST_ASSERT_EQUAL_INT(0, err);
+    TEST_ASSERT_EQUAL_UINT64(0, i);
+
+    err = partake_proquint_to_uint64("zuzuz-zuzuz-zuzuz-zuzuz", &i);
+    TEST_ASSERT_EQUAL_INT(0, err);
+    TEST_ASSERT_EQUAL_UINT64(-1, i);
+
+    err = partake_proquint_to_uint64("gutih-tugad-mudof-sakat", &i);
+    TEST_ASSERT_EQUAL_INT(0, err);
+    TEST_ASSERT_EQUAL_UINT64(0x3F54DCC18C62C18D, i);
+}
+
+
+void test_invalid_proquint(void) {
+    uint64_t i;
+    int err;
+
+    err = partake_proquint_to_uint64("babab-babab-babab-baba", &i);
+    TEST_ASSERT_NOT_EQUAL_INT(0, err);
+
+    err = partake_proquint_to_uint64("babab-babab-babab-babab-", &i);
+    TEST_ASSERT_NOT_EQUAL_INT(0, err);
+
+    err = partake_proquint_to_uint64("Babab-babab-babab-babab", &i);
+    TEST_ASSERT_NOT_EQUAL_INT(0, err);
+
+    err = partake_proquint_to_uint64("babab-babab.babab-babab", &i);
+    TEST_ASSERT_NOT_EQUAL_INT(0, err);
+
+}
+
+
+void test_roundtrip(void) {
+    uint64_t i, j;
+    char pq[24];
+    int err;
+
+    uint64_t samples[] = {
+        -2, -1, 0, 1, 2, 0xF0, 0xF00, 0xF000, 0xF0000,
+    };
+
+    for (int n = 0; n < sizeof(samples) / sizeof(uint64_t); ++n) {
+        i = samples[n];
+        partake_proquint_from_uint64(i, pq);
+        err = partake_proquint_to_uint64(pq, &j);
+        TEST_ASSERT_EQUAL_INT(0, err);
+        TEST_ASSERT_EQUAL_UINT64(i, j);
+    }
+}
+
+
 int main() {
     UNITY_BEGIN();
+    RUN_TEST(test_to_proquint);
+    RUN_TEST(test_16_from_proquint);
+    RUN_TEST(test_from_proquint);
+    RUN_TEST(test_invalid_proquint);
+    RUN_TEST(test_roundtrip);
     return UNITY_END();
 }
